f52e1179a989a90d0c2a556e8999eebc6b40285e
daiyarinzu <aii.luv.illusion@gmail.com>
2025-06-13 22:14:05 +0800

    Mobile Responsive Update u1.2

--- script.js
+++ script.js
@@ -20,15 +20,6 @@ function getRandomItem(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-// Fix mobile 100vh issue
-function setVhUnit() {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-}
-window.addEventListener("resize", setVhUnit);
-window.addEventListener("orientationchange", setVhUnit);
-window.addEventListener("load", setVhUnit);
-
 function scrollToBottom() {}
 
 // Capitalize names properly
